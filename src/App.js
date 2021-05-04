f58bb015a0014bc2c6d12392e6941325269b7b23
Afrosiyob <afrosiyob.a97@gmail.com>
2021-05-04 08:57:45 +0500

    add
    adad

--- src/App.js
+++ src/App.js
@@ -4,38 +4,19 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchData } from "./redux/test/action";
 
-function App() {
+function App () {
   const dispatch = useDispatch();
-
-  // const delay = (ms) => {
-  //   return new Promise((resolve) =>
-  //     setTimeout(() => {
-  //       resolve();
-  //     }, ms)
-  //   );
-  // };
 
   const url = "https://jsonplaceholder.typicode.com/todos";
 
-  // const fetchData = async () => {
-  //   try {
-  //     await delay(2000);
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  useEffect(() => {
-    dispatch(fetchData(url));
-  });
+  useEffect( () => {
+    dispatch( fetchData( url ) );
+  } );
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
