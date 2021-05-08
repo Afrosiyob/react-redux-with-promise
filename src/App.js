edd627e02fb65aa381ab9cf4d9c5388cc55da4fe
Afrosiyob <afrosiyob.a97@gmail.com>
2021-05-09 00:47:03 +0500

    add
    add

--- src/App.js
+++ src/App.js
@@ -3,47 +3,39 @@ import "./App.css";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchData } from "./redux/test/action";
+import { useFormInput } from "./hooks/useFormInput";
 
-function App() {
+
+function App () {
   const dispatch = useDispatch();
-
   const url = "https://jsonplaceholder.typicode.com/todos";
+  useEffect( () => {
+    dispatch( fetchData( url ) );
+  }, [ dispatch ] );
 
-  useEffect(() => {
-    dispatch(fetchData(url));
-  });
 
-  const Animals = function (options) {
-    this.name = options.name;
-    this.color = options.color;
-  };
 
-  const dog = new Animals({ name: "Rex", color: "#fff" });
+  const name = useFormInput( "" );
 
-  Animals.prototype.voice = function () {
-    console.log("this is voice of", this.name);
-  };
 
-  console.log(dog);
+  console.log( name );
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ logo } className="App-logo" alt="logo" />
+
+
+
+        <input type="text" { ...name } />
+
+
+
       </header>
     </div>
   );
 }
 
 export default App;
+
