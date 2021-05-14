7836c909bc07c519bb007f6acce71656df53cd58
Afrosiyob <afrosiyob.a97@gmail.com>
2021-05-14 19:05:31 +0500

    add
    add

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchData } from "./redux/test/action";
 
-import ChildComponent from "./components/ChildComponent";
-import { useState } from "react";
-import { useMemo } from "react";
-import Todo from "./components/Todo";
-import Link from "./components/Link/Link";
+
 
 
 
@@ -18,50 +14,16 @@ function App () {
     dispatch( fetchData( url ) );
   }, [ dispatch ] );
 
-  const [ count, setCount ] = useState( 0 );
-
-  const array = useMemo( () => {
-    return [ "One", "Two", "Three" ];
-  }, [] );
 
 
 
-  const todos = [
-    {
-      id: 1,
-      title: "open door",
-      completed: false
-    },
-    {
-      id: 2,
-      title: "wash dishes",
-      completed: true
-    }
-  ]
+
 
   return (
     <div className="App">
       <header className="App-header">
 
-
-        <Link title="this is title" />
-
-
-        {
-          todos.map( ( todo, index ) => (
-            <Todo todo={ todo } key={ index } />
-          ) )
-        }
-
-        <h1> Count is { count } </h1>
-        <button
-          type="button"
-          onClick={ () => setCount( ( prevCount ) => prevCount + 1 ) }
-        >
-          { " " }
-          Increment Count{ " " }
-        </button>
-        <ChildComponent title="This is title..." array={ array } />
+        this is header
       </header>{ " " }
     </div>
   );
