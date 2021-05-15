f83115b47f322d945699c38ceb0d475425c4fa02
Afrosiyob <afrosiyob.a97@gmail.com>
2021-05-15 13:17:36 +0500

    addd
    add

--- src/redux/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { testReducer } from "./test/reducer";
-
-export const rootReducer = combineReducers({
-  testReducer,
-});
--- src/redux/sagas.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import testSaga from "./test/saga";
-
-export default function* rootSaga() {
-  yield all([testSaga()]);
-}
