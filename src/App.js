38e44fa0645586bc11b6db7d979fae35fc5edd75
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-05-20 14:33:28 +0500

    add
    add

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import "./App.css";
-import ApiServices from "./redux/services/api";
-import { useEffect, useState } from "react";
-import Todo from "./components/Todo/Todo";
-import Counter from "./components/Counter/Counter";
-import Hello from "./components/Hello/Hello";
-import Form from "./components/Form/Form";
-import Fetch from "./components/Fetch/Fetch";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [state] = useState({
-    todos: [
-      {
-        label: "todo 1",
-        completed: true,
-      },
-      {
-        label: "todo 2",
-        completed: false,
-      },
-      {
-        label: "todo 3",
-        completed: false,
-      },
-    ],
-  });
-
-  const { testData } = new ApiServices();
-
-  const url = "https://jsonplaceholder.typicode.com/todos";
-
-  useEffect(() => {
-    testData(url)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-  const { todos } = state;
-
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
-        <Fetch url="/greeting" />
-
-        {todos?.map((todo, index) => (
-          <Todo key={index} label={todo.label} completed={todo.completed} />
-        ))}
-        <Counter number={0} />
-        <Hello />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
